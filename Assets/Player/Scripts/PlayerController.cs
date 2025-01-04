dc84736bcc465fc7c754309cf9a6160ce0d68cfd
SnippleRifsky <42582187+SnippleRifsky@users.noreply.github.com>
2025-01-04 14:38:36 -0500

    Cleanup
    Organize code and removed unused Camera reference

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -4,21 +4,24 @@ namespace Player.Scripts
 {
     public class PlayerController : MonoBehaviour
     {
+        private CharacterController _characterController;
+        private InputHandler _inputHandler;
+
+        [Header("Movement Parameters")] 
         [SerializeField] private float walkSpeed = 3f;
+        
+        [Header("Jump Parameters")]
         [SerializeField] private float jumpForce = 3f;
         [SerializeField] private float gravity = -9.81f;
         private Vector3 _currentMovement;
-        [SerializeField] public float smoothTime = 0.2f;
 
-        private CharacterController _characterController;
-        private InputHandler _inputHandler;
-        private Camera _camera;
+        [Header("Rotation Parameters")] [SerializeField]
+        public float smoothTime = 0.2f;
 
         private void Awake()
         {
             _characterController = GetComponent<CharacterController>();
             _inputHandler = InputHandler.Instance;
-            _camera = Camera.main;
         }
 
         private void Update()
