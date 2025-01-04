f38f4bd4dc112aef82c181c93729ca9a062ba96c
SnippleRifsky <42582187+SnippleRifsky@users.noreply.github.com>
2025-01-04 13:56:36 -0500

    Implement movement rotation

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Player.Scripts
@@ -9,14 +8,17 @@ namespace Player.Scripts
         [SerializeField] private float jumpForce = 3f;
         [SerializeField] private float gravity = -9.81f;
         private Vector3 _currentMovement;
-    
+        [SerializeField] public float smoothTime = 0.6f;
+
         private CharacterController _characterController;
         private InputHandler _inputHandler;
+        private Camera _camera;
 
         private void Awake()
         {
             _characterController = GetComponent<CharacterController>();
             _inputHandler = InputHandler.Instance;
+            _camera = Camera.main;
         }
 
         private void Update()
@@ -28,17 +30,17 @@ namespace Player.Scripts
         {
             HandleMovement();
             HandleJump();
+            HandleRotation();
         }
 
         private void HandleMovement()
         {
             var inputDirection = new Vector3(_inputHandler.MoveInput.x, 0, _inputHandler.MoveInput.y);
-            var worldDirection = transform.TransformDirection(inputDirection).normalized;
 
             if (_characterController.isGrounded)
             {
-                _currentMovement.x = worldDirection.x;
-                _currentMovement.z = worldDirection.z;
+                _currentMovement.x = inputDirection.x;
+                _currentMovement.z = inputDirection.z;
             }
 
             _characterController.Move(_currentMovement * (walkSpeed * Time.deltaTime));
@@ -49,15 +51,19 @@ namespace Player.Scripts
             if (_characterController.isGrounded)
             {
                 _currentMovement.y = -0.5f;
-                if (_inputHandler.JumpTriggered)
-                {
-                    _currentMovement.y = jumpForce;
-                }
+                if (_inputHandler.JumpTriggered) _currentMovement.y = jumpForce;
             }
             else
             {
                 _currentMovement.y += gravity * Time.deltaTime;
             }
+        }
+
+        private void HandleRotation()
+        {
+            if (_inputHandler.MoveInput == Vector2.zero) return;
+            var targetRotation = new Vector3(_inputHandler.MoveInput.x, 0, _inputHandler.MoveInput.y);
+            transform.forward = Vector3.Slerp(transform.forward, targetRotation, Time.deltaTime / smoothTime);
         }
     }
 }
