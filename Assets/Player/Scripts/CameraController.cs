67a55e3f90705062e87bf1f1ac36b478dabd1453
SnippleRifsky <shane_warren117@yahoo.com>
2025-01-06 17:12:02 -0500

    Implement Occlusion Detection
    Implemented Raycast between the player and camera, reset camera position to the hit position.

--- Assets/Player/Scripts/CameraController.cs
+++ Assets/Player/Scripts/CameraController.cs
@@ -1,28 +1,36 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
-using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
-    [Header("Camera Input")]
-    private InputAction _lookAction;
+    [Header("Camera Input")] private InputAction _lookAction;
     private InputAction _zoomAction;
-    
-    [Header("Camera Movement Parameters")]
-    [SerializeField] private float lookSensitivity = 0.5f;
+
+    [Header("Camera Movement Parameters")] [SerializeField]
+    private float lookSensitivity = 0.5f;
+
     [SerializeField] private float minPitch = -80f;
     [SerializeField] private float maxPitch = 80f;
     private Camera _camera;
     private float _currentX;
     private float _currentY;
-    
-    [Header("Camera Zoom Parameters")]
-    [SerializeField] private float minZoom = 2f;
+
+    [Header("Camera Zoom Parameters")] [SerializeField]
+    private float minZoom = 2f;
+
     [SerializeField] private float maxZoom = 20f;
     private float _distance;
     private float _zoomValue = 10f;
+
+    [Header("Camera Collision Parameters")] [SerializeField]
+    private LayerMask layerMask;
+    
+    private readonly float _distanceOffset = -0.2f;
+    private readonly Vector3 _rayDirectionYOffset = new(0f, -0.2f, 0f);
+    private RaycastHit _hit;
+
+    [Header("Debug Variables")] [SerializeField]
+    private bool isDebug;
 
     private void Awake()
     {
@@ -35,19 +43,20 @@ public class CameraController : MonoBehaviour
     {
         CameraRotation();
         Zoom();
-    } 
+        DetectOcclusion();
+    }
 
     private void CameraRotation()
     {
-        Vector2 lookInput = _lookAction.ReadValue<Vector2>();
-        
+        var lookInput = _lookAction.ReadValue<Vector2>();
+
         _currentX += lookInput.x * lookSensitivity;
         _currentY += lookInput.y * lookSensitivity;
 
         _currentY = Mathf.Clamp(_currentY, minPitch, maxPitch);
 
-        Vector3 direction = new Vector3(0, 0, -_distance);
-        Quaternion rotation = Quaternion.Euler(_currentY, _currentX, 0);
+        var direction = new Vector3(0, 0, -_distance);
+        var rotation = Quaternion.Euler(_currentY, _currentX, 0);
         _camera.transform.position = gameObject.transform.position + rotation * direction;
 
         _camera.transform.LookAt(gameObject.transform.position);
@@ -59,5 +68,17 @@ public class CameraController : MonoBehaviour
         _zoomValue += zoomInput.y * -1;
         _zoomValue = Mathf.Clamp(_zoomValue, minZoom, maxZoom);
         _distance = _zoomValue;
+    }
+
+    private void DetectOcclusion()
+    {
+        var playerPosition = gameObject.transform.position;
+        var camRay = new Ray(playerPosition,
+            (_camera.transform.position - gameObject.transform.position + _rayDirectionYOffset).normalized);
+
+        if (isDebug) Debug.DrawRay(camRay.origin, camRay.direction * (_distance + _distanceOffset), Color.red);
+
+        if (!Physics.Raycast(camRay, out _hit, _distance + _distanceOffset, layerMask)) return;
+        _camera.transform.position = _hit.point + new Vector3(0f, 0.2f, 0f);
     }
 }
