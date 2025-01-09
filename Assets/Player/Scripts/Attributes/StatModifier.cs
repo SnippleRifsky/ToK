d7d940dba6a81073d2059508d0ff296c5d2ab6b0
SnippleRifsky <shane_warren117@yahoo.com>
2025-01-09 03:05:27 -0500

    Update StatModifier.cs

--- Assets/Player/Scripts/Attributes/StatModifier.cs
+++ Assets/Player/Scripts/Attributes/StatModifier.cs
@@ -1,21 +1,34 @@
 public enum StatModType
 {
-   Flat,
-   Percent,
+    Flat = 100,
+    PercentMult = 200,
+    PercentAdd = 300,
 }
 
 public class StatModifier
 {
-   public readonly float Value;
-   public readonly StatModType Type;
-   public readonly int Order;
+    public readonly float Value;
+    public readonly StatModType Type;
+    public readonly int Order;
+    public readonly object Source;
 
-   public StatModifier(float value, StatModType type, int order)
-   {
-      Value = value;
-      Type = type;
-      Order = order;
-   }
+    public StatModifier(float value, StatModType type, int order, object source)
+    {
+        Value = value;
+        Type = type;
+        Order = order;
+        Source = source;
+    }
 
-   public StatModifier(float value, StatModType type) : this(value, type, (int)type) { }
+    public StatModifier(float value, StatModType type) : this(value, type, (int)type, null)
+    {
+    }
+
+    public StatModifier(float value, StatModType type, int order) : this(value, type, order, null)
+    {
+    }
+
+    public StatModifier(float value, StatModType type, object source) : this(value, type, (int)type, source)
+    {
+    }
 }
