dac9b374df44fdc9dbdb4f2522d1cf09607a67a9
SnippleRifsky <shane_warren117@yahoo.com>
2025-02-03 12:06:17 -0500

    Refactor NameplateManager for readability and maintainability.
    Restructured the class into logical regions for better organization and added helper methods to simplify code readability. Improved variable names for clarity and introduced constants for code reuse, ensuring a cleaner and more maintainable implementation.

--- Assets/Gameplay Components/Systems/Utilities/Managers/NameplateManager.cs
+++ Assets/Gameplay Components/Systems/Utilities/Managers/NameplateManager.cs
@@ -1,73 +1,111 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class NameplateManager : MonoBehaviour
 {
-        private Canvas _canvas;
-        
-        [SerializeField]
-        public GameObject uiEntityNameplatePrefab;
-        private readonly Queue<UIEntityNameplate> _nameplatePool = new();
-        private readonly Dictionary<Entity, UIEntityNameplate> _activeNameplates = new();
-        
-        public void Initialize()
-        {
-                _canvas = UIManager.Instance.UICanvas;
-        }
-        
-        public void ShowEntityNameplate(Entity entity)
-        {
-                if (_activeNameplates.ContainsKey(entity)) return;
+    private Canvas _uiCanvas;
 
-                var nameplate = GetNameplateFromPool();
-                nameplate.Setup(entity);
-                UpdateNameplatePosition(nameplate, entity);
-                _activeNameplates[entity] = nameplate;
-        }
-        
-        public void HideEntityNameplate(Entity entity)
-        {
-                if (_activeNameplates.TryGetValue(entity, out var nameplate))
-                {
-                        ReturnNameplateToPool(nameplate);
-                        _activeNameplates.Remove(entity);
-                }
-        }
-        private UIEntityNameplate GetNameplateFromPool()
-        {
-                if (_nameplatePool.Count > 0) return _nameplatePool.Dequeue();
+    [SerializeField]
+    private GameObject _entityNameplatePrefab;
 
-                var go = Instantiate(uiEntityNameplatePrefab, _canvas.transform).GetComponent<UIEntityNameplate>();
-                return go;
-        }
-        private void ReturnNameplateToPool(UIEntityNameplate nameplate)
-        {
-                nameplate.Clear();
-                nameplate.gameObject.SetActive(false);
-                _nameplatePool.Enqueue(nameplate);
-        }
-        
-        private void UpdateNameplatePosition(UIEntityNameplate nameplate, Entity entity)
-        {
-                var worldPosition = entity.transform.position;
+    private readonly Queue<UIEntityNameplate> _nameplatePool = new();
+    private readonly Dictionary<Entity, UIEntityNameplate> _activeNameplates = new();
 
-                var collider = nameplate.GetCachedCollider();
-                if (collider is null) return;
-                worldPosition.y += collider.bounds.extents.y;
+    private const float MIN_SCREEN_DEPTH = 0f;
 
-                var screenPosition = GameManager.Instance.PlayerCamera.WorldToScreenPoint(worldPosition);
-                nameplate.transform.position = screenPosition;
-                nameplate.gameObject.SetActive(screenPosition.z > 0);
-        }
-        
-        private void Update()
+    #region Initialization
+
+    public void Initialize()
+    {
+        _uiCanvas = UIManager.Instance.UICanvas;
+    }
+
+    #endregion
+
+    #region Nameplate Management
+
+    public void ShowEntityNameplate(Entity entity)
+    {
+        if (_activeNameplates.ContainsKey(entity)) return;
+
+        var nameplate = GetNameplateFromPool();
+        nameplate.Setup(entity);
+        UpdateNameplatePosition(nameplate, entity);
+        _activeNameplates[entity] = nameplate;
+    }
+
+    public void HideEntityNameplate(Entity entity)
+    {
+        if (_activeNameplates.TryGetValue(entity, out var nameplate))
         {
-                foreach (var kvp in _activeNameplates)
-                {
-                        var entity = kvp.Key;
-                        var nameplate = kvp.Value;
-                        UpdateNameplatePosition(nameplate, entity);
-                }
+            ReturnNameplateToPool(nameplate);
+            _activeNameplates.Remove(entity);
         }
+    }
+
+    #endregion
+
+    #region Helper Methods
+
+    private UIEntityNameplate GetNameplateFromPool()
+    {
+        if (_nameplatePool.Count > 0)
+        {
+            return _nameplatePool.Dequeue();
+        }
+
+        var newNameplate = Instantiate(_entityNameplatePrefab, _uiCanvas.transform)
+            .GetComponent<UIEntityNameplate>();
+        return newNameplate;
+    }
+
+    private void ReturnNameplateToPool(UIEntityNameplate nameplate)
+    {
+        nameplate.Clear();
+        nameplate.gameObject.SetActive(false);
+        _nameplatePool.Enqueue(nameplate);
+    }
+
+    private void UpdateNameplatePosition(UIEntityNameplate nameplate, Entity entity)
+    {
+        var screenPosition = CalculateScreenPosition(entity, nameplate);
+        nameplate.transform.position = screenPosition;
+        nameplate.gameObject.SetActive(screenPosition.z > MIN_SCREEN_DEPTH);
+    }
+
+    private Vector3 CalculateScreenPosition(Entity entity, UIEntityNameplate nameplate)
+    {
+        var worldPosition = entity.transform.position;
+
+        var collider = nameplate.GetCachedCollider();
+        if (collider != null)
+        {
+            worldPosition.y += collider.bounds.extents.y;
+        }
+
+        return GameManager.Instance.PlayerCamera.WorldToScreenPoint(worldPosition);
+    }
+
+    #endregion
+
+    #region Update Logic
+
+    private void Update()
+    {
+        UpdateAllNameplatePositions();
+    }
+
+    private void UpdateAllNameplatePositions()
+    {
+        var playerCamera = GameManager.Instance.PlayerCamera;
+
+        foreach (var kvp in _activeNameplates)
+        {
+            var entity = kvp.Key;
+            var nameplate = kvp.Value;
+            UpdateNameplatePosition(nameplate, entity);
+        }
+    }
+
+    #endregion
 }
