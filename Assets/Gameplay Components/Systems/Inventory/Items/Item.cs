acb2c24cf2e81e75367b9021571d5008d556c765
SnippleRifsky <shane_warren117@yahoo.com>
2025-02-06 17:37:29 -0500

    Add base Item class for inventory system
    Introduced the `Item` ScriptableObject to represent individual items within the inventory system. Includes basic properties such as ID, name, value, and icon, along with a CreateAssetMenu attribute for easy creation. Added necessary meta files for Unity compatibility.

---
+++ Assets/Gameplay Components/Systems/Inventory/Items/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "RPG Components/Inventory/Item", order = 0)]
+public class Item : ScriptableObject
+{
+    public int itemId;
+    public string itemName;
+    public int itemValue;
+    public Sprite itemIcon;
+}
